707e97ec9eaa518992b7f682ed849ec6218305f6
Ji-Wen-ZHuo <871812927@qq.com>
2022-03-22 22:59:37 +0800

    增加了导入配置文件的功能

--- src/main/java/com/jwz/controller/SiteController.java
+++ src/main/java/com/jwz/controller/SiteController.java
@@ -5,15 +5,14 @@ import com.jwz.layui.LayuiJSON;
 import com.jwz.pojo.Site;
 import com.jwz.service.SiteService;
 import com.jwz.service.SiteServiceImplSQL;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
@@ -33,7 +32,8 @@ public class SiteController {
     }
 
     @RequestMapping(value = "/site/{name}/{url}/{cookie}", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
-    public String addSite(@PathVariable String name, @PathVariable String url, @PathVariable String cookie) {
+    public String addSite(@PathVariable String name, @PathVariable String url, @PathVariable String cookie) throws UnsupportedEncodingException {
+        name = new String(name.getBytes("ISO-8859-1"), "utf-8");
         url = url.replace('-', '/');
         cookie = cookie.replace('@', ';');
         try {
@@ -90,5 +90,21 @@ public class SiteController {
         out.close();
 
         return null;
+    }
+
+    @RequestMapping(value = "/site/recover", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public String recover(@RequestParam("file") CommonsMultipartFile file , HttpServletRequest request) throws IOException {
+        InputStream in = file.getInputStream();
+        StringBuffer buffer = new StringBuffer();
+        int tmp;
+        while((tmp = in.read()) != -1) {
+            buffer.append((char)tmp);
+        }
+        String config = buffer.toString();
+        List<Site> sites = JSON.parseArray(config, Site.class);
+        for (Site site : sites) {
+            new SiteServiceImplSQL().addSite(site.getName(), site.getUrl(), site.getCookie());
+        }
+        return LayuiJSON.addHeader("{\"src\": \"http://cdn.layui.com/123.jpg\"}");
     }
 }
